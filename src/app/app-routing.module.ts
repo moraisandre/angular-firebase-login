1bf0c3c7b7d72144fccd756d7478bb3c41af3bdd
André Morais <andrewinvista@gmail.com>
2020-10-12 11:26:46 -0300

    Created dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent,
+  },
+  {
+    path: 'home',
+    component: DashboardComponent,
   },
 ];
 
